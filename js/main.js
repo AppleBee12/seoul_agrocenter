ab0a42ec1adcf03d8b6edfad8c8f9274646d71f9
AppleBee12 <mieldeluna@naver.com>
2024-08-18 03:05:29 +0900

    search완성

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 //search
 const search_btn = document.querySelector('.search-btn')
 const search = document.querySelector('#searchbox')
+const search_clear = document.querySelector('.search-clear')
 
 search_btn.addEventListener('click',()=>{
-  
- search.classList.toggle('active');
+ search.classList.add('active');
 });
 
+search_clear.addEventListener('click',()=>{
+    search.classList.remove('active');
+   });
