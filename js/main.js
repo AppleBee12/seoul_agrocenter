d1f0d77915e4bf6b67ab9c653ed401b423d76e83
AppleBee12 <mieldeluna@naver.com>
2024-08-21 03:40:51 +0900

    마지막저장

--- js/main.js
+++ js/main.js
@@ -2,26 +2,51 @@
 let menu = document.querySelector('header .container nav');
 let header = document.querySelector('header .container');
 
-menu.addEventListener('mouseenter',()=>{
-	header.style.height = '635px';
+menu.addEventListener('mouseenter', () => {
+    header.style.height = '635px';
 });
-menu.addEventListener('mouseleave',()=>{
-	header.style.height = '81px';
+menu.addEventListener('mouseleave', () => {
+    header.style.height = '81px';
 })
 
 
 //search
-const search_btn = document.querySelector('.search-btn')
-const search = document.querySelector('#searchbox')
-const search_clear = document.querySelector('.search-clear')
+const search_btn = document.querySelector('.search-btn');
+const search = document.querySelector('#searchbox');
+const search_clear = document.querySelector('.search-clear');
 
-search_btn.addEventListener('click',()=>{
- search.classList.add('active');
+search_btn.addEventListener('click', () => {
+    search.classList.add('active');
 });
 
-search_clear.addEventListener('click',()=>{
+search_clear.addEventListener('click', () => {
     search.classList.remove('active');
-   });
+});
+
+
+
+//tab
+const tabMenus = document.querySelectorAll('.noticewrap .tabmenu li');
+const tabContents = document.querySelectorAll('.tabcontent div');
+
+tabMenus.forEach(tabMenu => {
+    tabMenu.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        for (let menu of tabMenus) {
+            menu.classList.remove('active');
+        }
+        tabMenu.classList.add('active');
+
+        let target = tabMenu.querySelector('a').getAttribute('href');
+        for (let content of tabContents) {
+            content.classList.remove('active');
+        }
+        document.querySelector(target).classList.add('active');
+    });
+})
+
+
 
 
 //slide
